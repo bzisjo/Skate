beab7fd4dd38ad9df8a121d9d64814f09d49d984
qianma3 <divagnes01@gmail.com>
2017-04-05 16:28:37 -0500

    arduino code change

--- arduino_bluetooth_testing/master/master.ino
+++ arduino_bluetooth_testing/master/master.ino
@@ -5,13 +5,14 @@
  *                 == MASTER CODE ==
  */
 
-#define ledPin 9
-
+#define ledPin 9 //
+#define statusLED 6
 int state = 0;
 int pot = 0;
 int trans_status = 0;
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(statusLED, OUTPUT);
   //digitalWrite(ledPin, LOW);
   Serial.begin(38400); // Default communication rate of the Bluetooth module
 }
@@ -21,25 +22,33 @@ void loop() {
     trans_status = 1;
     state = Serial.read(); // Reads the data from the serial port
  }
+ if(trans_status)
+ digitalWrite(statusLED, HIGH);
  // Controlling the LED
-// if (state == '1') {
- if (trans_status == 1) {  
+//Serial.print(state,DEC);
+//Serial.print('\n');
+ if (state == '1') {
+// if (trans_status == 1) {  
   digitalWrite(ledPin, HIGH); // LED ON
   state = 0;
  }
-// else if (state == '0') {
-else if (trans_status == 0) {
-  digitalWrite(ledPin, LOW); // LED ON
+ else if (state == '0') {
+//else if (trans_status == 0) {
+  digitalWrite(ledPin, LOW); // LED OFF
   state = 0;
  }
  // Reading the potentiometer
  pot = digitalRead(2);
  //int potValueMapped = map(potValue, 0, 1023, 0, 255);
- //Serial.print(pot);
- if(pot)
+
+ if(pot){
  Serial.write('1'); // Sends potValue to servo motor
- else
+// Serial.print('\n');
+}
+ else{
  Serial.write('0');
+// Serial.print('\n');
+}
  delay(10);
  
 }
--- arduino_bluetooth_testing/slave/slave.ino
+++ arduino_bluetooth_testing/slave/slave.ino
@@ -6,47 +6,59 @@
  */
 
 #include <SoftwareSerial.h>
-#define ledPin 9
-#define button 8
-#define error 10
-//Servo myServo;
+
+#define errorLED 10
+#define dataLED 9
+#define statusLED 8
+#define button 7
+
 int state = 20;
 int buttonState = 0;
 
 void setup() {
   pinMode(button, INPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(error, OUTPUT);
+  pinMode(dataLED, OUTPUT);
+  pinMode(errorLED, OUTPUT);
+  pinMode(statusLED, OUTPUT);
+  //digitalWrite(statusLED, LOW);
   //myServo.attach(9);
   Serial.begin(38400); // Default communication rate of the Bluetooth module
 }
 
 void loop() {
+ digitalWrite(statusLED,LOW);
+ digitalWrite(errorLED, LOW);
+ digitalWrite(dataLED, LOW);
  if(Serial.available() > 0){ // Checks whether data is comming from the serial port    
-    state = Serial.read(); // Reads the data from the serial port
+   state = Serial.read(); // Reads the data from the serial port
+   digitalWrite(statusLED, HIGH);
  }
- // Controlling the servo motor
+//Serial.print(state,DEC);
+//Serial.print('\n');
+// Controlling the servo motor
   if (state == '1') {
-  digitalWrite(ledPin, LOW); // LED ON
-  state = 0;
- }
- else if (state == '0') {
-  digitalWrite(ledPin, LOW); // LED ON
-  state = 0;
- }
- else 
- digitalWrite(error, HIGH);
+    digitalWrite(dataLED, HIGH); // LED ON
+    state = 0;
+  }
+  else if (state == '0') {
+    digitalWrite(dataLED, LOW); // LED ON
+    state = 0;
+  }
+  else 
+    digitalWrite(errorLED, HIGH);
  //Serial.print(state);
  //Serial.print('\n');
 
  delay(10);
  
- // Reading the button
+/* // Reading the button
  buttonState = digitalRead(button);
  if (buttonState == HIGH) {
    Serial.write('1'); // Sends '1' to the master to turn on LED
  }
  else {
   Serial.write('0');
- }  
+ }
+*/
+ /*delay(1000);*/
 }
