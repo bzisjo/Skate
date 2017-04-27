2a38e75932bc5217b20d2d0cda6f7b39d4dc2c17
Jonathan Wang <jywang6@Illinois.edu>
2017-04-27 01:14:53 -0500

    Added masterdata (2)

--- main_program/main_program/mpu6050/mpu6050.h
+++ main_program/main_program/mpu6050/mpu6050.h
@@ -38,7 +38,7 @@ References:
 //definitions for raw data
 //gyro and acc scale
 #define MPU6050_GYRO_FS MPU6050_GYRO_FS_2000
-#define MPU6050_ACCEL_FS MPU6050_ACCEL_FS_2
+#define MPU6050_ACCEL_FS MPU6050_ACCEL_FS_4
 
 #define MPU6050_GYRO_LSB_250 131.0
 #define MPU6050_GYRO_LSB_500 65.5
